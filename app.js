bd2cfeb802d304e250ee650f4cbcbe15a7f5a5d8
vanssign <thindvansh1999@gmail.com>
2021-02-15 10:36:17 +0530

    cors added

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
+var cors = require("cors");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -13,7 +14,7 @@ var redirectRouter = require ("./routes/redirects")
 var app = express();
 
 //mongo connection
-// const mongoUrl = 
+const mongoUrl = 'mongodb+srv://vanssign:mandysandy1718@cluster0.08ywr.mongodb.net/ushotify?retryWrites=true&w=majority';
 const connect = mongoose.connect(mongoUrl, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -31,6 +32,8 @@ connect
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
+//enabled all cors request
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
